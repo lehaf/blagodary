525c510bb5b0709cb652928e8a2198f222807d7f
alexf <a.f@webcompany.by>
2023-07-30 22:02:12 +0300

    create settings tables for referral system

---
+++ local/modules/webcompany.referal.system/classes/mysql/WebcompanyReferralSystem.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace WebCompany;
+
+use \Bitrix\Main\Application;
+
+final class WebcompanyReferralSystem
+{
+    private string $tabsTable = 'w_referrals_tabs';
+    private string $groupsTable = 'w_referrals_groups';
+    private string $fieldsTable = 'w_referrals_fields';
+    private string $defaultSettingsFilePath = '/local/modules/webcompany.referal.system/.default.php';
+    private int $standardSort = 500;
+    private object $db;
+
+    public function __construct()
+    {
+        $this->db = Application::getConnection();
+    }
+//    public function getSettings()
+//    {
+//        $result = $this->db->query(
+//        "SELECT * FROM $this->tabsTable".
+//        );
+//        while($ar = $result->fetch())
+//        {
+//            pr($ar);
+//        }
+//    }
+
+    public function setDefaultSettings()
+    {
+        if (file_exists($_SERVER['DOCUMENT_ROOT'].$this->defaultSettingsFilePath)) {
+            $defaultSettings = require_once $_SERVER['DOCUMENT_ROOT'].$this->defaultSettingsFilePath;
+            if (!empty($defaultSettings)) {
+                $sqlTabsInsert = 'INSERT INTO '.$this->tabsTable.' (CODE, NAME, TITLE, SORT) VALUES ';
+                $lastTabKey = array_key_last($defaultSettings);
+                $sqlGroupsInsert = 'INSERT INTO '.$this->groupsTable.' (TAB_ID, CODE, NAME, SORT) VALUES ';
+                $sqlFieldsInsert = 'INSERT INTO '.$this->fieldsTable.' (GROUP_ID, NAME, VALUE, HINT, EXPLANATION, TYPE, LENGHT, HEIGHT, SORT) VALUES ';
+                foreach ($defaultSettings as $tabCode => $tabInfo) {
+                    static $tabId = 1;
+                    $sqlTabsInsert .= "('$tabCode', '$tabInfo[tabName]', '$tabInfo[tabName]', $this->standardSort)";
+                    if ($lastTabKey === $tabCode) {
+                        $sqlTabsInsert.= ";\r\n";
+                    } else {
+                        $sqlTabsInsert.= ', ';
+                    }
+
+                    if (!empty($tabInfo['tabGroups'])) {
+                        $lastGroupKey = array_key_last($tabInfo['tabGroups']);
+                        foreach ($tabInfo['tabGroups'] as $groupCode => $groupInfo) {
+                            static $groupId = 1;
+                            $sqlGroupsInsert .= "($tabId, '$groupCode', '$groupInfo[groupName]', $this->standardSort)";
+
+                            if ($lastTabKey === $tabCode && $lastGroupKey === $groupCode) {
+                                $sqlGroupsInsert.= ";\r\n";
+                            } else {
+                                $sqlGroupsInsert.= ', ';
+                            }
+
+                            if (!empty($groupInfo['settings'])) {
+                                $lastFieldKey = array_key_last($groupInfo['settings']);
+                                foreach ($groupInfo['settings'] as $key => $fieldInfo) {
+                                    $sqlFieldsInsert .= "($groupId, '$fieldInfo[name]', '$fieldInfo[default]', '$fieldInfo[hint]',
+                                    '$fieldInfo[explanation]', '".$fieldInfo['type'][0]."', ".($fieldInfo['type'][1] ?? "NULL").", ".
+                                        ($fieldInfo['type'][2] ?? "NULL").", $this->standardSort)";
+
+                                    if ($lastTabKey === $tabCode && $lastGroupKey === $groupCode && $lastFieldKey === $key) {
+                                        $sqlFieldsInsert.= ';';
+                                    } else {
+                                        $sqlFieldsInsert.= ', ';
+                                    }
+                                }
+                            }
+                            $groupId++;
+                        }
+                    }
+                    $tabId++;
+                }
+                $sqlRequest = $sqlTabsInsert.$sqlGroupsInsert.$sqlFieldsInsert;
+                $this->db->queryExecute($sqlRequest);
+            }
+        }
+    }
+}
--- local/modules/webcompany.referal.system/include.php
+++ local/modules/webcompany.referal.system/include.php
@@ -4,5 +4,6 @@ Bitrix\Main\Loader::registerAutoloadClasses(
     'webcompany.referal.system',
     array(
         'WebCompany\WReferralsTable' => 'lib/WReferralsTable.php',
+        'WebCompany\WebcompanyReferralSystem' => 'classes/mysql/WebcompanyReferralSystem.php',
     )
 );
