f1223bca4787300e497382a9d274558ba87176f1
alexf <a.f@webcompany.by>
2023-09-02 16:41:50 +0300

    create news.list template for socials and create socials infoclock

---
+++ local/templates/.default/components/bitrix/news.list/socials/result_modifier.php
@@ -0,0 +1,23 @@
+<?php
+
+//if (!empty($arResult['ITEMS'][0])) {
+//    // Всегда обрабатываем только первый элемент
+//    $arItem = &$arResult['ITEMS'][0];
+//    // Генерируем правильное склонение для числа
+//    $arVariants = [
+//        'вещь',
+//        'вещи',
+//        'вещей',
+//    ];
+//    $arItem['PROPERTIES']['COUNTER']['SKLONEN'] = sklonen((int)$arItem['PROPERTIES']['COUNTER']['VALUE'],$arVariants);
+//    // Собираем числов вида 35 557 вместо 35557
+//    $reversCounter = strrev($arItem['PROPERTIES']['COUNTER']['VALUE']);
+//    $arReversCounter = implode(' ',str_split($reversCounter, 3));
+//    $arItem['PROPERTIES']['COUNTER']['VALUE'] = strrev($arReversCounter);
+//    // Ресайзим картинку
+//    $arItem['DETAIL_PICTURE'] = CFile::ResizeImageGet(
+//        $arItem['DETAIL_PICTURE']['ID'],
+//        array("width" => 900, "height" => 100),
+//        BX_RESIZE_IMAGE_PROPORTIONAL,
+//    );
+//}
