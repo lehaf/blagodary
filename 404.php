d04b5ed1b1c0e0bd0fb145ba6d010b67f67f24f1
lehaf <lehaforever9@bk.ru>
2024-01-04 17:30:52 +0300

    create 404 page

--- 404.php
+++ 404.php
@@ -1,8 +1,19 @@
 <?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+
+/** @global object  $APPLICATION */
+
+use Bitrix\Main\Page\Asset;
+
+Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/html/css/404.css");
+
 CHTTP::SetStatus("404 Not Found");
 @define("ERROR_404","Y");
-$APPLICATION->SetTitle("404 Not Found");
-
-echo 404;
-
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+$APPLICATION->SetTitle("Ничего не найдено!");
+$APPLICATION->SetPageProperty("NOT_SHOW_NAV_CHAIN", "Y");
+?>
+<div class="container">
+    <div class="error-code">4<span class="heart">♡</span>4</div>
+    <div class="error-text">Такой страницы не существует</div>
+    <a class="btn-bg back-to-main" href="/">На главную</a>
+</div>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
