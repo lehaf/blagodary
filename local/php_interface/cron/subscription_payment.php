48931eee0fc15a2caf1f7dca0e3904f69fabc959
lehaf <lehaforever9@bk.ru>
2024-05-25 01:04:55 +0300

    edit cron file for subscription

--- local/php_interface/cron/subscription_payment.php
+++ local/php_interface/cron/subscription_payment.php
@@ -1,5 +1,7 @@
 <?php
 
+$_SERVER['DOCUMENT_ROOT'] = '/home/blagodarub/public_html';
+
 require_once($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_before.php');
 
 use WebCompany\Subscription;
