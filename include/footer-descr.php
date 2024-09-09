f054ddedec1e09745bf3717d7ea1813135d56eb3
webcompany <webcompany@mail.by>
2024-09-09 03:33:44 +0300

    server changes

--- include/footer-descr.php
+++ include/footer-descr.php
@@ -1,12 +1,12 @@
-БлагоДарю — крупнейшая площадка объявлений Использование ресурса БлагоДарю означает согласие с
-<a class="text-page__link" href="/agreement/">пользовательским соглашением</a> и <a class="text-page__link" href="/policy/">политикой обработки персональных данных</a>.<br><br>
-
-
-
-ИНДИВИДУАЛЬНЫЙ ПРЕДПРИНИМАТЕЛЬ АСТАШКЕВИЧ ВИТАЛИЙ ИГОРЕВИЧ<br>
-Адрес: РБ, Г. МИНСК, УЛ. СТАРИНОВСКАЯ, Д. 37 ОФ. 2<br>
-УНП: 192042278<br>
-03.09.2013г<br><br>
-
-Контактный номер телефона: +375 29 196 0914<br>
-Email: info@blagodaru.by
+БлагоДарю — крупнейшая площадка объявлений Использование ресурса БлагоДарю означает согласие с <a class="text-page__link" href="/agreement/">пользовательским соглашением</a> и <a class="text-page__link" href="/policy/">политикой обработки персональных данных</a>.<br>
+ <br>
+ <span style="font-size: 6pt;">ИНДИВИДУАЛЬНЫЙ ПРЕДПРИНИМАТЕЛЬ АСТАШКЕВИЧ ВИТАЛИЙ ИГОРЕВИЧ</span><br>
+<span style="font-size: 6pt;"> </span><span style="font-size: 6pt;">
+Адрес: РБ, Г. МИНСК, УЛ. СТАРИНОВСКАЯ, Д. 37 ОФ. 2</span><br>
+<span style="font-size: 6pt;"> </span><span style="font-size: 6pt;">
+УНП: 192042278</span><br>
+<span style="font-size: 6pt;"> </span><span style="font-size: 6pt;">
+03.09.2013г</span><br>
+ <br>
+ Контактный номер телефона: +375 29 196 0914<br>
+ Email: <a href="mailto:info@blagodaru.by">info@blagodaru.by</a>
